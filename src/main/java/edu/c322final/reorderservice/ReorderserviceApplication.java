3eafc0349b57f6136706196cf751ca6e7a8df80a
Zachary Young <zhyoung@iu.edu>
2023-04-14 16:13:08 -0400

    Initial commit

---
+++ src/main/java/edu/c322final/reorderservice/ReorderserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.c322final.reorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReorderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReorderserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/c322final/reorderservice/ReorderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.c322final.reorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReorderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
